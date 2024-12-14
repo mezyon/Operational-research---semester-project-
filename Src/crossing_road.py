61bf94ee5de45a04330ac760d06dabf152a1af1a
udemer <127339091+KirkH4mmett@users.noreply.github.com>
2024-12-14 17:29:36 +0100

    parametryzacja algorytmu i zamknięcie działania w jednej funkcji

--- Src/crossing_road.py
+++ Src/crossing_road.py
@@ -2,6 +2,7 @@ import random
 from copy import deepcopy
 from math import inf
 import matplotlib.pyplot as plt
+import numpy as np
 
 possible_combinations = {
     0: [1, 1, 0, 0, 0, 0, 0, 0],
@@ -18,6 +19,28 @@ possible_combinations = {
     11: [1, 0, 0, 0, 1, 0, 0, 0]
 }
 
+# lista opcji transformacji - w obecnej implementacji można ją zmieniać na [0], [1] oraz [0, 1]
+# nie można zrezygnować z operatora krzyżowania
+mut_opt = [0, 1]
+perm_opt = [0, 1]
+cros_opt = [0, 1]
+
+# współczynniki do liczenia funkcji celu
+base_glob = 1   # dodawane w każdym kroku
+a_glob = 3    # mnożnik kary
+b_glob = 1    # wykładnik kary
+c_glob = 1      # kara za pozostałe auta
+
+# prawdopodobieństwo wystąpienia mutacji (i permutacji)
+mut_probability = 0.5
+perm_probability = mut_probability
+
+# dodanie pięciu losowych rozwiązań do listy utworzonej z krzyżowania
+add_random_5 = True
+
+# liczba itearcji bez poprawy do warunku stopu
+no_improvement = 40
+
 
 class Simulation:
 
@@ -27,6 +50,7 @@ class Simulation:
         self.ni = None
         self.t_list = None
         self.best_solution_history = []
+        self.best_solution_in_population = []
         self.best_quality = 9999999999999999999999999999
 
     def get_random_startpoint(self, a=2, b=5):
@@ -36,16 +60,20 @@ class Simulation:
     def get_test_startpoint(self):
         self.n_vect_start = [2, 1, 2, 1, 2, 1, 2, 1]
 
-    def get_population(self, quantity, length=12):
+    def get_population(self, quantity, length=12, dummy=0):
         self.population = [Solution(length) for _ in range(quantity)]
-        for el in self.population:
-            el.randomize()
+        if dummy == 1:
+            for el in self.population:
+                el.dummy()
+        else:
+            for el in self.population:
+                el.randomize()
 
     def calculate_solution_quality(self, solution):
-        base_value = 3
-        a_ = 0.2
-        b_ = 0.3
-        c_ = 5
+        base_value = base_glob
+        a_ = a_glob
+        b_ = b_glob
+        c_ = c_glob
         quality = 0
         N = deepcopy(self.n_vect_start)
         n_matrix = [[] for _ in range(len(solution.solution) + 1)]
@@ -103,11 +131,14 @@ class Simulation:
         solution.quality = quality
         # print(n_list)
 
-    def genetic_algorithm(self, quantity=100, length=7, iterations=100):
+    def genetic_algorithm(self, quantity=100, length=7, iterations=100, desired_solution=0, MUT_=mut_probability, PERM_=perm_probability, add_random_5_inside=add_random_5, dummy=0):
+
+
         # stwórz pierwszą populację
-        self.get_population(quantity, length)
+        self.get_population(quantity, length, dummy)
         counter = 0
-        while counter < iterations:
+        improvement_flag = True
+        while counter < iterations and improvement_flag:
             counter += 1
 
             # oblicz jakość w populacji
@@ -119,37 +150,52 @@ class Simulation:
 
             # posortuj rozwiązania względem jakości
             self.population.sort(key=lambda x: x.quality, reverse=False)
+            # aktualizacja listy do tej pory najlepszego rozwiąznia
             try:
                 self.best_solution_history.append(self.population[0].quality if self.population[0].quality < self.best_solution_history[-1] else self.best_solution_history[-1])
             except IndexError:
                 self.best_solution_history.append(self.population[0].quality)
+            # aktualizacja listy najlepszego rozwiązania w obecnej populacji
+            self.best_solution_in_population.append(self.population[0].quality)
 
-            TOP_25 = deepcopy(self.population[:quantity//4])
+            # wybierz jakościowo najlepsze 25% populacji do krosowania
+            crossing_population = deepcopy(self.population[:quantity//4])
 
-            crossing = []
-            for el in TOP_25:
-                for _ in range(2): # wykonaj dwa razy
-                    one, two = el.crossing(random.choice(TOP_25))
-                    crossing.append(one)
-                    crossing.append(two)
+            # dodaj pięć losowych rozwiązań
+            if add_random_5_inside:
+                for _ in range(5):
+                    random_solution = Solution(length)
+                    random_solution.randomize()
+                    crossing_population.append(random_solution)
 
-            # 1/4 ilości populacji razy wykonaj mutacje
-            for _ in range(quantity//4):
-                idx = random.randint(0, len(crossing) - 1)
-                crossing[idx].mutation(random.choice([0, 1]))
-                idx = random.randint(0, len(crossing) - 1)
-                crossing[idx].permutation(random.choice([0, 1]))
+            new_population = []
+            for el in crossing_population:
+                for _ in range(2):   # wykonaj dwa razy
+                    one, two = el.crossing(random.choice(crossing_population), random.choice(cros_opt))
+                    new_population.append(one)
+                    new_population.append(two)
 
-            self.population = crossing
+            # mutacje w nowo utworzonej populacji
+            for idx in range(len(new_population)):
+                if random.random() < MUT_:
+                    new_population[idx].mutation(random.choice(mut_opt))
+                if random.random() < PERM_:
+                    new_population[idx].permutation(random.choice(perm_opt))
+
+            self.population = new_population
+
+            # warunki stopu działają niejako dopiero po no_improvement + 10 iteracji
+            if len(self.best_solution_history) > (no_improvement + 10):
+                if abs(np.mean(self.best_solution_history[-no_improvement:-1]) - self.best_solution_history[-1]) < 0.01:
+                    improvement_flag = False
+                if desired_solution > self.best_solution_history[-1]:
+                    improvement_flag = False
 
         for one_sol in self.population:
             self.calculate_solution_quality(one_sol)
 
         # posortuj rozwiązania względem jakości
         self.population.sort(key=lambda x: x.quality, reverse=False)
-
-        # for el in self.population[:5]:
-        #     print(el.quality)
 
 
 class Solution:
@@ -161,6 +207,9 @@ class Solution:
         self.solution = [None for _ in range(length)]  # lista do przechowywania roziązania
         self.quality = 999999999  # duża i charakterystyczna liczba, ale nie nieskończoność dla lepszej diagnostyki
         self.length = length
+
+    def dummy(self):
+        self.solution = [0 for _ in range(len(self.solution))]
 
     def randomize(self):
         self.solution = [random.randint(0, 11) for _ in range(len(self.solution))]
@@ -214,6 +263,7 @@ class Solution:
         """
         metoda krzyżująca, przyjmuje inne rozwiązanie i krzyżuje je z tym
         :param other: inne rozwiązanie
+        typ1: wybiera losowo miejsce przecięcia i względem niego wykonuje krzyżowanie
         :return: (new_sol1, new_sol2)
         """
         new_sol1 = Solution(self.length)
@@ -224,65 +274,92 @@ class Solution:
             new_sol1.solution = self.solution[:bound] + other.solution[bound:]
             new_sol2.solution = other.solution[:bound] + self.solution[bound:]
 
+        elif typ == 1:
+            # Krzyżowanie "w kratkę"
+            new_sol1.solution = [self.solution[i] if i % 2 == 0 else other.solution[i] for i in range(self.length)]
+            new_sol2.solution = [other.solution[i] if i % 2 == 0 else self.solution[i] for i in range(self.length)]
+
         return new_sol1, new_sol2
 
 
+def przeprowadzenie_symulacji(wektor_poczatkowy=None,
+                              dlugosc_rozwiazania=None,
+                              rozmiar_populacji=100,
+                              liczba_iteracji=100,
+                              wartosc_progowa=0,
+                              a=2,
+                              b=5,
+                              mut=mut_probability,
+                              perm=perm_probability,
+                              add=add_random_5,
+                              dummy=0):
+    """
+
+    :param wektor_poczatkowy: sytuacja na skrzyżowaniu do rozważenia
+    :param dlugosc_rozwiazania:
+    :param rozmiar_populacji: rozmiar populacji w każdym kroku algorytmu
+    :param liczba_iteracji: warunek stopu - jest to MAKSYMALNA liczba iteracji
+    :param wartosc_progowa: warunek stopu - domyślnie ustawiony na zero, czyli nieaktywny. Działa
+                            dopiero po pierwszych dwudziestu wykonanych iteracjach
+    :param a: minimalna ilość pojazdów w danej kolejce (jeśli wektor poczatkowy równy None)
+    :param b: maksymalna ilość pojazdów w danej kolejce (jeśli wektor poczatkowy równy None)
+    prawdop. mutacji
+    prawdop. permutacji
+    dodawanie lsoowych 5ciu rozwiązań do każdej populacji
+    dummy - start nie z punktu losowego, tylko z samych zer
+    :return: nie zwraca nic, tylko printuje i rysuje wykresy
+    """
+    symulka = Simulation()
+    if wektor_poczatkowy is None:
+        # ustalić losowy wektor początkowy
+        symulka.get_random_startpoint(a, b)
+    else:
+        symulka.n_vect_start = wektor_poczatkowy
+    if dlugosc_rozwiazania is None:
+        # ustalić (optymalną) długość rozwiązania
+        dlugosc_rozwiazania = sum(symulka.n_vect_start) // 4
+
+    print("pozycja początkowa:", symulka.n_vect_start)
+    print("Długość rozwiązania", dlugosc_rozwiazania)
+
+    symulka.genetic_algorithm(rozmiar_populacji, dlugosc_rozwiazania, liczba_iteracji, wartosc_progowa, mut, perm, add, dummy)
+
+    przebieg_najlepszej_f_celu = symulka.best_solution_history
+    przebieg_f_celu = symulka.best_solution_in_population
+    print("Funkcja celu najlepszego rozwiąznia", min(przebieg_najlepszej_f_celu))
+    print("Najlepsze rozwiązanie:", symulka.population[0].solution)
+
+
+    # WYKRES PRZEBIEGU FUNKCJI CELU NAJLEPSZEGO ROZWIĄZANIA EVER
+    plt.plot(range(len(przebieg_najlepszej_f_celu)), przebieg_najlepszej_f_celu)
+    plt.title(f"{symulka.n_vect_start}, rozwiązanie dł. {dlugosc_rozwiazania}")
+    plt.xlabel("Iteracja")
+    plt.ylabel("Wartość Funkcji celu")
+    plt.grid()
+    plt.show()
+
+    # WYKRES PRZEBIEGU FUNKCJI CELU NAJLEPSZEGO ROZWIĄZANIA W DANEJ ITERACJI
+    plt.plot(range(len(przebieg_f_celu)), przebieg_f_celu)
+    plt.title(f"{symulka.n_vect_start}, rozwiązanie dł. {dlugosc_rozwiazania}")
+    plt.xlabel("Iteracja")
+    plt.ylabel("Wartość Funkcji celu")
+    plt.grid()
+    plt.show()
+
+
 if __name__ == '__main__':
-    # Algorytm wykonuje pełną ilość iteracji i dopiero kończy,
-    # nie ma jeszcze zaimplementowanych dodatkowych warunków stopu takich jak:
-    # * uzyskanie wystarczająco dobrego rozwiązania
-    # * brak polepszenia funkcji celu przez daną ilość iteracji
-    # * wykonywanie algorytmu przez określoną ilość czasu
 
     test1_start = [2, 5, 2, 2, 4, 4, 2, 2, 2]
-    sim = Simulation()
-    sim.n_vect_start = test1_start
-    sim.genetic_algorithm(100, 12, 40)
-    data_test1 = sim.best_solution_history
 
-    sim = Simulation()
-    sim.get_random_startpoint(6, 12)
-    start = sim.n_vect_start
-    print("pozycja początkowa:", start)
-    dlugosc_rozwiazania = sum(start) // 4
-    print("Długość rozwiązania", dlugosc_rozwiazania)
-
-    sim.genetic_algorithm(100, dlugosc_rozwiazania, 40)
-
-    data_test2 = sim.best_solution_history
-
-    plt.plot(range(len(data_test1)), data_test1)
-    plt.title(f"{test1_start}, rozwiązanie dł. 12")
-    plt.xlabel("Iteracja")
-    plt.ylabel("Wartość Funkcji celu")
-    plt.grid()
-    plt.show()
-
-    plt.plot(range(len(data_test2)), data_test2)
-    plt.title(f"{start}, rozwiązanie dł. {dlugosc_rozwiazania}")
-    plt.xlabel("Iteracja")
-    plt.ylabel("Wartość Funkcji celu")
-    plt.grid()
-    plt.show()
-
-    test3_start = [0, 0, 0, 0, 80, 0, 0, 0, 0]
-
-    sim = Simulation()
-    sim.n_vect_start = test3_start
-    start = sim.n_vect_start
-    print("pozycja początkowa:", start)
-    dlugosc_rozwiazania = sum(start) // 4
-    print("Długość rozwiązania", dlugosc_rozwiazania)
-
-    sim.genetic_algorithm(100, dlugosc_rozwiazania, 40)
-
-    data_test3 = sim.best_solution_history
-
-    print(sim.population[0].solution)
-
-    plt.plot(range(len(data_test3)), data_test3)
-    plt.title(f"{start}, rozwiązanie dł. {dlugosc_rozwiazania}")
-    plt.xlabel("Iteracja")
-    plt.ylabel("Wartość Funkcji celu")
-    plt.grid()
-    plt.show()
+    test3_start = [0, 0, 0, 40, 40, 0, 0, 0, 0]
+    przeprowadzenie_symulacji(wektor_poczatkowy=None,
+                              dlugosc_rozwiazania=20,
+                              rozmiar_populacji=100,
+                              liczba_iteracji=1000,
+                              wartosc_progowa=0,
+                              a=18,
+                              b=35,
+                              mut=0.3,
+                              perm=0.3,
+                              add=False,
+                              dummy=0)
